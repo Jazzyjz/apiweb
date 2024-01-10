82f41559919e64b37a589dccd549a73d8e942df1
MugenJigo <os.romero1995@gmail.com>
2024-01-10 03:38:22 -0500

    added button

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,5 @@
 var newsKey = 'f93aeec5d3ac4aab85825744ef51f35a'
 
-var foodUrl = 'www.themealdb.com/api/json/v1/1/random.php'
 
 //JASMINS CODE
 
@@ -13,3 +12,22 @@ var foodUrl = 'www.themealdb.com/api/json/v1/1/random.php'
 
 
 //OSCARS CODE
+
+var btn = document.querySelector('.btn')
+btn.addEventListener('click',generateFood)
+
+function generateFood (){
+    var foodUrl='www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata'
+    var vnum =
+
+    fetch(foodUrl)
+       
+        .then(function(response){
+            if(response.ok){
+                console.log('connnection recieved')
+            }
+            
+            console.log(response)
+        
+    })
+};
